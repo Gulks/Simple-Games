4258a050b61b933a3fb9f909fcf45d75ad43cf00
Gulks <42969088+Gulks@users.noreply.github.com>
2022-02-13 23:56:19 +0700

    Add files via upload

---
+++ check_phrases.py
@@ -0,0 +1,28 @@
+import re
+from time import sleep
+
+def reverse(a):
+    """Reverses the phrase.
+    
+    Excludes all the symbols except for the letters and 
+    makes them in lower case."""
+
+    a = re.sub("[^A-Za-z]", "", a)
+    a = a.lower()
+    return a[::-1]
+
+
+def is_palindrome(a):
+    return a == reverse(a)
+
+
+phrase = input("Enter a phrase and I'll check if it is a polindrome: ")
+phrase = re.sub("[^A-Za-z]", "", phrase)
+phrase = phrase.lower()
+
+if (is_palindrome(phrase)):  
+    sleep(1)
+    print("Yes, your phrase is a polindrome.")
+else:
+    sleep(2)
+    print("No, your phrase isn't a polindrome.")
