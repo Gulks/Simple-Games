e594ed693d59f685554d21920edbb8a990e2e5cb
Gulks <42969088+Gulks@users.noreply.github.com>
2022-02-17 20:15:23 +0700

    Add files via upload
    ValueError не срабатывает. Всё остальное нормуль. Мой вариант для отчаявшихся - написать для юзера сразу "Если введешь число - оно не сохранится в списке имен." И ещё: запись в файл идет тупая, т.е. список превратился в строку, но это можно исправить, думаю, на изи. Главное сейчас для меня - понять, почему не могу поймать ValueError

---
+++ users_copy.py
@@ -0,0 +1,111 @@
+import re 
+from time import sleep
+
+users_names = []
+
+def ask_users_names():
+    while True: 
+        try: 
+            name = str(input("\nWhat is your name? "))
+            
+        except ValueError:
+            print("Sorry, you can't enter numbers, try again please: ")
+            ask_users_names() 
+
+        else: 
+            global users_names
+            name = re.sub("[^A-Za-z]", "", name) 
+            users_names.append(name.title())
+            ask_to_continue()  
+
+
+def ask_to_continue(): 
+    """Asks a user if they want to contunue entering names."""
+
+    answer = input("\nDo you want to continue? ")
+
+    if answer == 'no':
+        sleep(1)
+        confirmation = input("\nDo you want to see the list of users' names? ")
+
+        if confirmation == 'no' or confirmation == 'n':
+            save_users_names()
+            sleep(1)
+            print("\nOk, thanks for using our program. Good bye!") 
+            quit()
+
+        else:
+            save_users_names() 
+            show_names() 
+
+    else:
+        ask_users_names() 
+
+
+def save_users_names():
+    """Users' names are saved in a separate text file."""
+
+    global users_names 
+    new_list = ''.join(users_names)
+    f = open('users_names.txt', 'w')
+    f.write(new_list)    
+    f.close() 
+
+
+def show_names():   
+    """The users' names are shown.
+    
+    If there are several names, they're enumerated with comma 
+    due to another function - 'enumerate_names'."""
+
+    global users_names
+
+    while '' in users_names:
+        users_names.remove('')
+
+    users_names = list(users_names)
+
+    if len(users_names) == 0:
+        sleep(1)
+        print("\nThere are no names in the list.") 
+        sleep(1)
+        print("\nThanks for using our program. Good bye!\n")
+        quit()
+
+    elif len(users_names) == 1:
+        sleep(1)
+        print("\nThere is only one user name - {}.".format(users_names[0]))
+        sleep(1) 
+        print("\nIt is saved in a separate text document. Good bye!\n") 
+        quit()
+
+    elif len(users_names) == 2:
+        sleep(1)
+        print("\nThere are two names in the list - {0} and " \
+            "{1}.".format(users_names[0], users_names[1])) 
+        sleep(1)
+        print("\nThey are saved in a separate text document. Good bye!\n") 
+        quit()
+
+    else:
+        enumerate_names()
+        sleep(1)
+        print("\nThey are saved in a separate text document. "\
+            "If you want to read them, open the file 'users_names'. "\
+            "Good bye!\n") 
+        quit()
+
+
+def enumerate_names():
+    """Enumerares users names with comma and does not depict the last one (comma)."""
+
+    popped_name = users_names.pop(-1)
+    print("The users names are: ", end='')
+    for u_name in users_names:
+        print(u_name, end=', ') 
+    print(popped_name, end='')
+    print(".") 
+
+
+
+ask_users_names()
